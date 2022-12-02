4b09748694808b2b16d78eb2f2c3f5ea9cc042d1
hoanglocdn95@gmail.com <hoanglocdn95@gmail.com>
2022-12-03 00:24:37 +0700

    fix bug

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Format Content - Format Everything
+
+## Usage
+
+formatTextUppercase: convert string text to uppercase
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const typeOf = value => Object.prototype.toString.call(value).slice(8, -1)
 
 const formatTextUppercase = (string) => {
-  if (typeOf(string) === 'string') return string.toUpperCase();
+  if (typeOf(string) === 'String') return string.toUpperCase();
   return console.error('Value with wrong type pass through formatTextUppercase, expect string type')
 }
 
